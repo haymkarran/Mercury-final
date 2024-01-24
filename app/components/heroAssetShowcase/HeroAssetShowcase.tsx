c1eb1ee75ccad752489a0b3743dadda23b23c119
haymkarran <40350866+haymkarran@users.noreply.github.com>
2024-01-24 12:37:28 +0400

    Add files via upload

--- app/components/heroAssetShowcase/HeroAssetShowcase.tsx
+++ app/components/heroAssetShowcase/HeroAssetShowcase.tsx
@@ -21,7 +21,7 @@ const HeroAssetShowcase: React.FC = () => {
                 />
                 <div className='centered'>
                     <h1 className='text-h3 mb-4'>Built Locally, Connected Globally</h1>
-                    <p className='text-paragraph'>Founded in the UAE, Mercury is a multifaceted payments ecosystem enabler fostering growth & digitalization. We are a technology-driven, solution-oriented team with decades of experience in the payments domain. We are on a mission to democratize financial services, level the playing field and drive cashless ecosystems.</p>
+                    <p className='text-paragraph'>Founded in the UAE, Mercury designs, deploys, and manages Digital Payment ecosystems that enable sovereignty and inclusion. We are a technology-driven, solution-oriented team with decades of experience in the payments domain. We are on a mission to democratize financial services, level the playing field and drive cashless ecosystems.</p>
                 </div>
             </motion.div>
         </>
